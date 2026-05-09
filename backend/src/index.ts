ccd17ac6408d4c2813fd2cbba37f18e15b104046
dyredhead <danielyentin@gmail.com>
2026-05-09 17:35:19 -0400

    added CORS functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-import {env} from './env.js'
+import express from "express";
+import cors from "cors";
+import { env } from "./env.js";
 // import healthRouter from './routes/health.ts';
 // import usersRouter from './routes/users.js';
 // import { usersContract } from '@my-app/shared';
@@ -7,10 +8,11 @@ import {env} from './env.js'
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // app.use(healthRouter);
 // app.use(usersContract.mount, usersRouter);
 
 app.listen(env.express_port, () => {
-  console.log(`Backend running at http://localhost:${env.express_port}`);
+    console.log(`Backend running at http://localhost:${env.express_port}`);
 });
